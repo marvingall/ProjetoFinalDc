8d9ac538d085b9cf4e65d342bc02004a8a1f70b9
jsdaniell <jose.daniell@outlook.com>
2024-08-07 21:38:26 -0300

    new screen edit and add employee

--- dashboard-alert-panel/src/components/SideMenuItem/SideMenuItem.jsx
+++ dashboard-alert-panel/src/components/SideMenuItem/SideMenuItem.jsx
@@ -1,7 +1,18 @@
-export default function SideMenuItem({title = "Dashboard", icon}) {
+
+import { useRouter } from "next/navigation";
+
+export default function SideMenuItem({title = "Dashboard", icon, path}) {
     const Icon = icon;
 
-    return <div className="cursor-pointer min-w-full p-2 bg-green-700 max-h-10 text-white rounded text-sm flex items-center">
+    const router = useRouter();
+
+    function goToPath() {
+        if (path) {
+            router.push(path);
+        }
+    }
+
+    return <div onClick={goToPath} className="cursor-pointer min-w-full p-2 bg-green-700 max-h-10 text-white rounded text-sm flex items-center">
     {icon && <div className="mr-2">
                 <Icon size={20} />
         </div>}
